570f73e87460660abce31260f4f389b24a060094
BaronChang <fantasyregina@foxmail.com>
2016-08-29 22:48:30 +0800

    Create sizeofdata.cpp
    lalala,自己写的！

---
+++ sizeofdata.cpp
@@ -0,0 +1,24 @@
+//数据大小显示
+//@BaronChang,铮琪大人，allrightresevered
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "This program can tell you the sizes of different types of datas in your computer.";//UI information
+	system("pause");
+	/*TODO:fix char*/
+	const	short theSht = 1;		//
+	const	int theInt = 1;			//declare const
+	const	long theLng = 1;		//**********
+
+	cout << "the size of char:    " << "?" << "\n"
+		 << "the size of short:   " << sizeof(theSht) << "\n"
+		 << "the size of int:     " << sizeof(theInt) << "\n"
+		 << "the size of long:    " << sizeof(theLng) << "\n";
+	system("pause");
+	return 0;
+}
+
