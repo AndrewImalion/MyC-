f3c448e00e1e9b402cca4e5df7d1bcc09b31ec1c
BaronChang <fantasyregina@foxmail.com>
2016-09-02 22:54:56 +0800

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,22 @@
+//使用while计算阶乘
+#include"stdafx.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "阶乘计算" << endl;
+	kaitou:long sum =1;
+	int j = 1;
+	long i;
+	cout << "input a number pls."<<endl;
+	cin >> i;
+	while(j<=i)
+	{
+		sum = sum*j;
+		j++;
+	};
+	cout << i << "!=" <<sum<< endl;
+	goto kaitou;
+	system("pause");
+	return 0;
+}
